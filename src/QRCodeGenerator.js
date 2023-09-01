2ef67df69979917ff0dd1489f9443260b71ebfaa
Jeong-Yunseo <216yoon@gmail.com>
2023-09-01 18:25:23 +0900

    final...plz

--- src/QRCodeGenerator.js
+++ src/QRCodeGenerator.js
@@ -1,16 +1,102 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import QRCode from 'qrcode-generator';
+// import './background.css'
+
+// const QRCodeGenerator = () => {
+//   const [name, setName] = useState('');
+//   const [number, setNumber] = useState('');
+//   const [part, setPart] = useState('');
+//   const [qrCodeImage, setQRCodeImage] = useState('');
+
+//   const generateQRCode = () => {
+//     // const qrData = `이름: ${name}, 기수: ${number}, 파트: ${part}`;
+//     const qrData = `Name: ${name}, Number: ${number}, Part: ${part}`;
+//     const typeNumber = 4;
+//     const errorCorrectionLevel = 'L';
+
+//     const qr = QRCode(typeNumber, errorCorrectionLevel);
+//     qr.addData(qrData);
+//     qr.make();
+
+//     setQRCodeImage(qr.createDataURL(6, 10));
+//   };
+
+//   return (
+//     <div className='my-container'>
+//       <div>
+//         {/* <h1>QR Code Generator</h1> */}
+//         <div style={{margin: '15px'}}>
+//           <label htmlFor="name" style={{fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>이름(영문): </label>
+//           <input
+//             type="text"
+//             id="name"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             required
+//             style={{width: '250px', height: '20px'}}
+//           />
+//         </div>
+//         <div style={{margin: '15px'}}>
+//           <label htmlFor="number" style={{fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>기수(숫자): </label>
+//           <input
+//             type="text"
+//             id="number"
+//             value={number}
+//             onChange={(e) => setNumber(e.target.value)}
+//             required
+//             style={{width: '250px', height: '20px'}}
+//           />
+//         </div>
+//         <div style={{margin: '15px'}}>
+//           <label htmlFor="part" style={{fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>파트(영문): </label>
+//           <input
+//             type="text"
+//             id="part"
+//             value={part}
+//             onChange={(e) => setPart(e.target.value)}
+//             required
+//             style={{width: '250px', height: '20px'}}
+//           />
+//         </div>
+//         <button onClick={generateQRCode} style={{fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold', margin: '15px'}}>QR 코드 생성</button>
+//         <div>
+//           {qrCodeImage && (
+//             <img src={qrCodeImage} alt="Generated QR Code" />
+//           )}
+//         </div>
+
+//         <Link to="/qrcodescanner" style={{margin: '15px', fontSize: '20px', textDecoration: 'none', marginTop: '20px', display: 'block', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>
+//           QR 코드 스캐너
+//         </Link>
+//       </div>
+//       <div><br></br><br></br></div>
+//     </div>
+//   );
+// };
+
+// export default QRCodeGenerator;
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import QRCode from 'qrcode-generator';
-import './background.css'
+import './background.css';
 
 const QRCodeGenerator = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [part, setPart] = useState('');
   const [qrCodeImage, setQRCodeImage] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
 
   const generateQRCode = () => {
-    // const qrData = `이름: ${name}, 기수: ${number}, 파트: ${part}`;
+    if (!name || !number || !part) {
+      setShowAlert(true);
+      return;
+    }
+
+    setShowAlert(false);
+
     const qrData = `Name: ${name}, Number: ${number}, Part: ${part}`;
     const typeNumber = 4;
     const errorCorrectionLevel = 'L';
@@ -24,53 +110,57 @@ const QRCodeGenerator = () => {
 
   return (
     <div className='my-container'>
-      <div style={{padding: '15px', borderRadius: '10px', backgroundColor: 'rgba(255, 255, 255, 0.8)', textAlign: 'center', }}>
-        {/* <h1>QR Code Generator</h1> */}
-        <div>
-          <label htmlFor="name" style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>이름(영문): </label>
+      <div>
+        <div style={{ margin: '15px' }}>
+          <label htmlFor="name" style={{ fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold' }}>이름(영문): </label>
           <input
             type="text"
             id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
             required
-            style={{width: '200px', height: '20px'}}
+            style={{ width: '250px', height: '20px' }}
           />
         </div>
-        <div>
-          <label htmlFor="number" style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>기수(숫자): </label>
+        <div style={{ margin: '15px' }}>
+          <label htmlFor="number" style={{ fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold' }}>기수(숫자): </label>
           <input
             type="text"
             id="number"
             value={number}
             onChange={(e) => setNumber(e.target.value)}
             required
-            style={{width: '200px', height: '20px'}}
+            style={{ width: '250px', height: '20px' }}
           />
         </div>
-        <div>
-          <label htmlFor="part" style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>파트(영문): </label>
+        <div style={{ margin: '15px' }}>
+          <label htmlFor="part" style={{ fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold' }}>파트(영문): </label>
           <input
             type="text"
             id="part"
             value={part}
             onChange={(e) => setPart(e.target.value)}
             required
-            style={{width: '200px', height: '20px'}}
+            style={{ width: '250px', height: '20px' }}
           />
         </div>
-        <br></br>
-        <button onClick={generateQRCode} style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>QR 코드 생성</button>
-        <div>
-          {qrCodeImage && (
-            <img src={qrCodeImage} alt="Generated QR Code" />
-          )}
-        </div>
+        <button onClick={generateQRCode} style={{ fontSize: '20px', fontFamily: 'NanumGothic', fontWeight: 'bold', margin: '15px' }}>QR 코드 생성</button>
+        
+        {showAlert && (
+          <p style={{ color: 'red' }}>모든 필드를 입력하세요.</p>
+        )}
 
-        <Link to="/qrcodescanner" style={{fontSize: '25px', textDecoration: 'none', marginTop: '20px', display: 'block', fontFamily: 'NanumGothic', padding: '5px', backgroundColor: 'white', borderRadius: '5px', fontWeight: 'bold'}}>
+        {qrCodeImage && (
+          <div>
+            <img src={qrCodeImage} alt="Generated QR Code" />
+          </div>
+        )}
+
+        <Link to="/qrcodescanner" style={{ margin: '15px', fontSize: '20px', textDecoration: 'none', marginTop: '20px', display: 'block', fontFamily: 'NanumGothic', fontWeight: 'bold' }}>
           QR 코드 스캐너
         </Link>
       </div>
+      <div><br /><br /></div>
     </div>
   );
 };
--- src/QRCodeScanner.js
+++ src/QRCodeScanner.js
@@ -1,13 +1,84 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import QrScanner from 'react-qr-scanner';
+// import { Link } from 'react-router-dom';
+
+// const QRCodeScanner = () => {
+//   const [scannedData, setScannedData] = useState(null);
+
+//   const handleScan = (result) => {
+//     if (result) {
+//       setScannedData(result.text);
+//     }
+//   };
+
+//   const handleError = (error) => {
+//     console.error(error);
+//   };
+
+//   const handleRescan = () => {
+//     setScannedData(null); // 스캔 데이터 초기화하여 다시 스캔 가능하게
+//   }
+
+//   return (
+//     <div className='qr-scan-container' style={{marginLeft: '10px'}}>
+//       <h1 style={{marginLeft: '10px'}}>QR Code Scanner</h1>
+//       <QrScanner
+//         delay={300}
+//         onError={handleError}
+//         onScan={handleScan}
+//         style={{ width: '50%' }}
+//       />
+//       <div style={{ marginTop: '20px', fontSize: '20px' }}>
+//         {scannedData && (
+//           <>
+//             {/* <p>스캔된 데이터:</p>
+//             <p>{scannedData}</p> */}
+//             {/* 파싱된 데이터 화면에 표시 */}
+//             {scannedData.includes('Name') && scannedData.includes('Number') && scannedData.includes('Part') ? (
+//               <>
+//                 <p style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>이름: {scannedData.match(/Name: ([\w\s]+)/)[1]}</p>
+//                 <p style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>기수: {scannedData.match(/Number: ([\w\s]+)/)[1]}기</p>
+//                 <p style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>파트: {scannedData.match(/Part: ([\w\s]+)/)[1]}</p>
+//               </>
+//             ) : (
+//               <p>유효한 데이터가 아닙니다.</p>
+//             )}
+//           </>
+//         )}
+//       </div>
+//       {scannedData && (
+//         <button onClick={handleRescan} style={{fontSize: '20px', marginTop: '20px'}}>
+//             QR 코드 다시 스캔
+//         </button>
+//       )
+
+//       }
+//       <Link to="/" style={{fontSize: '25px', textDecoration: 'none', marginTop: '20px', display: 'block', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>
+//         QR 코드 생성기
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default QRCodeScanner;
+
+import React, { useState, useEffect } from 'react';
 import QrScanner from 'react-qr-scanner';
 import { Link } from 'react-router-dom';
 
 const QRCodeScanner = () => {
   const [scannedData, setScannedData] = useState(null);
+  const [showData, setShowData] = useState(false);
 
   const handleScan = (result) => {
     if (result) {
       setScannedData(result.text);
+      setShowData(true);
+
+      // 5초 후에 데이터 숨기기
+      setTimeout(() => {
+        setShowData(false);
+      }, 3000);
     }
   };
 
@@ -16,12 +87,20 @@ const QRCodeScanner = () => {
   };
 
   const handleRescan = () => {
-    setScannedData(null); // 스캔 데이터 초기화하여 다시 스캔 가능하게
-  }
+    setScannedData(null);
+    setShowData(false);
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 unmount 되면 타이머를 클리어합니다.
+    return () => {
+      clearTimeout();
+    };
+  }, []);
 
   return (
-    <div className='qr-scan-container' style={{marginLeft: '10px'}}>
-      <h1 style={{marginLeft: '10px'}}>QR Code Scanner</h1>
+    <div className='qr-scan-container' style={{ marginLeft: '10px' }}>
+      <h1 style={{ marginLeft: '10px' }}>QR Code Scanner</h1>
       <QrScanner
         delay={300}
         onError={handleError}
@@ -29,16 +108,18 @@ const QRCodeScanner = () => {
         style={{ width: '50%' }}
       />
       <div style={{ marginTop: '20px', fontSize: '20px' }}>
-        {scannedData && (
+        {showData && scannedData && (
           <>
-            {/* <p>스캔된 데이터:</p>
-            <p>{scannedData}</p> */}
-            {/* 파싱된 데이터 화면에 표시 */}
-            {scannedData.includes('Name') && scannedData.includes('Number') && scannedData.includes('Part') ? (
+            {scannedData.includes('Name') &&
+            scannedData.includes('Number') &&
+            scannedData.includes('Part') ? (
               <>
-                <p style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>이름: {scannedData.match(/Name: ([\w\s]+)/)[1]}</p>
-                <p style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>기수: {scannedData.match(/Number: ([\w\s]+)/)[1]}기</p>
-                <p style={{fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>파트: {scannedData.match(/Part: ([\w\s]+)/)[1]}</p>
+                <p style={{ fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold' }}>
+                  SIREN {scannedData.match(/Number: ([\w\s]+)/)[1]}기 {scannedData.match(/Part: ([\w\s]+)/)[1]} {scannedData.match(/Name: ([\w\s]+)/)[1]}님
+                </p>
+                <p style={{ fontSize: '25px', fontFamily: 'NanumGothic', fontWeight: 'bold' }}>
+                  SIREN 22기 정기 공연에 오신 것을 환영합니다!
+                </p>
               </>
             ) : (
               <p>유효한 데이터가 아닙니다.</p>
@@ -46,14 +127,20 @@ const QRCodeScanner = () => {
           </>
         )}
       </div>
-      {scannedData && (
-        <button onClick={handleRescan} style={{fontSize: '20px', marginTop: '20px'}}>
-            QR 코드 다시 스캔
-        </button>
-      )
-
-      }
-      <Link to="/" style={{fontSize: '25px', textDecoration: 'none', marginTop: '20px', display: 'block', fontFamily: 'NanumGothic', fontWeight: 'bold'}}>
+      <button onClick={handleRescan} style={{ fontSize: '20px', marginTop: '20px' }}>
+        QR 코드 다시 스캔
+      </button>
+      <Link
+        to="/"
+        style={{
+          fontSize: '25px',
+          textDecoration: 'none',
+          marginTop: '20px',
+          display: 'block',
+          fontFamily: 'NanumGothic',
+          fontWeight: 'bold',
+        }}
+      >
         QR 코드 생성기
       </Link>
     </div>
